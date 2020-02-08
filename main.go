8b3c30501e71b1bab397a2581b6c63bf1ba5524f
Felix Angell <felixangell@otzaf.org>
2020-02-08 13:07:13 +0000

    testing deployment

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"github.com/otz1/otz/cache"
-	"github.com/otz1/otz/controller"
+	"net/http"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/otz1/otz/cache"
+	"github.com/otz1/otz/controller"
 )
 
 func main() {
@@ -17,6 +19,10 @@ func main() {
 	}
 
 	router.GET("/search", ctrl.Search)
+	router.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "this is a test!")
+	})
+
 	router.Run(":8001")
 	cache.Close()
 }
