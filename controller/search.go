539a7e7605aae5e8ead0dc45fd074058ae26fe0a
Felix Angell <felixangell@otzaf.org>
2020-02-09 11:19:11 +0000

    return some dummy results that match what the frontend expects

--- controller/search.go
+++ controller/search.go
@@ -9,21 +9,34 @@ import (
 	"github.com/otz1/otz/api"
 )
 
+// SearchResult is an individual result
+// that is returned.
 type SearchResult struct {
-	Href string `json:"href"`
+	Title           string `json:"title"`
+	Snippet         string `json:"snippet"`
+	Ranking         int    `json:"ranking"`
+	ImageSource     string `json:"imageSource"`
+	ThumbnailSource string `json:"thumbnailSource"`
+	Href            string `json:"href"`
 }
 
+// MeasurementDetail is information that provides
+// how long the request took and how many results were given
 type MeasurementDetail struct {
+	// perhaps elapsed time could be a float?
 	ElapsedTime time.Duration `json:"elapsedTime"`
 	ResultCount uint64        `json:"resultCount"`
 }
 
+// SearchResponse ...
 type SearchResponse struct {
 	Query        string            `json:"query"`
 	Results      []SearchResult    `json:"results"`
 	Measurements MeasurementDetail `json:"measurements"`
+	NumPages     int               `json:"numPages"`
 }
 
+// Search ...
 func (ctx *Controller) Search(c *gin.Context) {
 	query := c.Query("query")
 
@@ -32,7 +45,14 @@ func (ctx *Controller) Search(c *gin.Context) {
 	api.ProcessSearch(query)
 
 	results := []SearchResult{
-		SearchResult{"http://google.com/"},
+		SearchResult{
+			Title:           "title",
+			Snippet:         "this is a snippet from the webpage",
+			Ranking:         1,
+			ImageSource:     "http://placehold.it/256x256",
+			ThumbnailSource: "http://placehold.it/256x256",
+			Href:            "https://felixangell.com",
+		},
 	}
 	c.JSON(http.StatusOK, SearchResponse{
 		Query:   query,
