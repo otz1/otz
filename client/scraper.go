c77e94231b53910d3f10b7a56e1bf81d34f118fe
Felix Angell <felixangell@otzaf.org>
2020-04-11 18:21:20 +0100

    added env variable to pickup environment

--- client/scraper.go
+++ client/scraper.go
@@ -1,13 +1,31 @@
 package client
 
 import (
+	"fmt"
 	"github.com/getsentry/sentry-go"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/otz1/otz/entity"
+	"github.com/otz1/otz/util"
 	"github.com/parnurzeal/gorequest"
 )
 
+var scrapeEndpoint = map[string]string{
+	"prod":    "https://otzs.otzaf.org/scrape",
+	"staging": "http://otzs.qa.otzaf.org/scrape",
+	// local speaks to QA.
+	"local": "http://otzs.qa.otzaf.org/scrape",
+}
+
 type ScraperClient struct{}
+
+func (s *ScraperClient) getEndpoint() string {
+	env := util.GetEnv("ENVIRONMENT", "local")
+	endpoint, ok := scrapeEndpoint[env]
+	if !ok {
+		sentry.CaptureException(fmt.Errorf("not such environment '%s'", env))
+	}
+	return endpoint
+}
 
 func (s *ScraperClient) Scrape(query string) *entity.ScrapeResponse {
 	scrapeRequest := entity.ScrapeRequest{
@@ -16,7 +34,7 @@ func (s *ScraperClient) Scrape(query string) *entity.ScrapeResponse {
 
 	_, body, errs := gorequest.
 		New().
-		Post("https://otzs.otzaf.org/scrape"). // TODO get from env variables.
+		Post(s.getEndpoint()).
 		AppendHeader("Content-Type", "application/json").
 		AppendHeader("Accept", "application/json").
 		AppendHeader("SITE-CODE", "OTZIT_UK"). // TODO deduce this rather than hardcode
--- main.go
+++ main.go
@@ -6,14 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/otz1/otz/cache"
 	"github.com/otz1/otz/controller"
+	"github.com/otz1/otz/util"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 func main() {
+	env := util.GetEnv("ENVIRONMENT", "local")
+	log.Println("RUNNING IN", strings.ToUpper(env))
+
 	err := sentry.Init(sentry.ClientOptions{
-		Dsn: "https://b8dd734fc88c43eb95110d9a1a06a2e1@sentry.io/5187014",
+		Dsn:         "https://b8dd734fc88c43eb95110d9a1a06a2e1@sentry.io/5187014",
+		Environment: env,
 	})
 	if err != nil {
 		log.Fatalf("sentry.Init: %s", err)
